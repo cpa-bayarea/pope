daba672b1819b04c53b0bdcfb2f9e8827ded7b7b
Kimura <joseduardo.kimura@gmail.com>
2018-07-28 10:28:46 -0300

    :wrench: Add CNPJ validators

--- organizations/validators.py
+++ organizations/validators.py
@@ -1,44 +1,28 @@
 """Custom validators for pope Organizations """
-import re
-
 from django.core.validators import EMPTY_VALUES
 from django.forms import ValidationError
 
 
-def DV_maker(v):
-    if v >= 2:
-        return 11 - v
-    return 0
+def get_digit(multiplier_arr, cnpj):
+    count = 0
+
+    for idx, num in enumerate(multiplier_arr):
+        count += num * int(list(cnpj)[idx])
+
+    digit = (11 - (count % 11))
+    if (count % 11) < 2:
+        return 0
+    return digit
 
 
 def validate_CNPJ(value):
-    """Validates CNPJ.
-    Reference: https://djangosnippets.org/snippets/10601/
-    """
+    if len(value) != 14:
+        raise ValidationError('O CNPJ deve conter 14 dígitos')
 
-    value = str(value)
-    if value in EMPTY_VALUES:
-        return u''
-    if not value.isdigit():
-        value = re.sub("[-/\.]", "", value)
-        orig_value = value[:]
-    try:
-        int(value)
-    except ValueError:
-        raise ValidationError('CNPJ deve conter apenas dígitos')
-    if len(value) > 14:
-        raise ValidationError('CNPJ Invalido')
-    orig_dv = value[-2:]
+    first_digit = get_digit([5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2], value)
+    second_digit = get_digit([6, 5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2], value)
 
-    new_1dv = sum([i * int(value[idx]) for idx, i in enumerate(list(
-        range(5, 1, -1)) + list(range(9, 1, -1)))])
-    new_1dv = DV_maker(new_1dv % 11)
-    value = value[:-2] + str(new_1dv) + value[-1]
-    new_2dv = sum([i * int(value[idx]) for idx, i in enumerate(list(
-        range(6, 1, -1)) + list(range(9, 1, -1)))])
-    new_2dv = DV_maker(new_2dv % 11)
-    value = value[:-1] + str(new_2dv)
-    if value[-2:] != orig_dv:
+    if value[-2:] != '{}{}'.format(first_digit, second_digit):
         raise ValidationError('CNPJ inválido')
-
-    return orig_value
+    
+    return value 
