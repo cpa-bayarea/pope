3d5cba3a25e76ec77e9e3f5146e0f9adafbee574
Kimura <joseduardo.kimura@gmail.com>
2018-08-04 11:06:25 -0300

    :bug: Fix confirmation password error throwing
    Fix #15

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views import View
+from django.contrib.auth.hashers import make_password
+from django.views.generic import TemplateView
 from django.views.generic.edit import FormView
 
 from users.forms import PopeUserForm
@@ -11,15 +12,27 @@ class PopeUserFormView(FormView):
     success_url = '/login'
 
     def form_valid(self, form):
-        form.save()
+        new_user = form.save(commit=False)
+        new_user.password = make_password(
+            form.cleaned_data.get('password')
+        )
+
+        new_user.save()
+
         return super().form_valid(form)
 
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        confirm_password = cleaned_data.get('confirm_password')
 
-class DashboardView(LoginRequiredMixin, View):
+        if password != confirm_password:
+            raise forms.ValidationError(
+                'Confirmação de senha não confere com a senha'
+            )
+
+
+class DashboardView(LoginRequiredMixin, TemplateView):
     login_url = '/login/'
     redirect_field_name = 'redirect_to'
     template_name = 'dashboard_prestador.html'
-
-    def get_template_name(self):
-        if self.request.user.user_type is 'P':
-            return 'dashboard_prestador.html'
