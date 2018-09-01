1aa270c23861afdd1087067c1d4d0a189e4acb39
Kimura <joseduardo.kimura@gmail.com>
2018-09-01 09:50:06 -0300

    :recycle: Remodel project
    Part of #30

--- organizations/models.py
+++ organizations/models.py
@@ -1,22 +1,36 @@
 """Django organizations customizations for pope """
 from django.db import models
 
-from organizations.validators import validate_CNPJ
-
 
 class Organization(models.Model):
-    # Atributes
+    """Organization model."""
     org_name = models.CharField(max_length=35, null=False,
                                 blank=False, default=False)
-    cnpj = models.CharField(max_length=15, null=False, unique=True,
-                            blank=False, default=False,
-                            validators=[validate_CNPJ])
-    social_reason = models.CharField(max_length=25, null=False,
-                                     blank=False, default=False)
     email = models.EmailField(null=False, blank=False,
                               unique=True, default=False)
     telephone = models.CharField(max_length=11, null=False,
                                  blank=False, default=False)
-    cep = models.CharField(max_length=8, null=False, blank=False)
+    cep = models.CharField(max_length=8, null=True, blank=True)
+    neighbourhood = models.CharField(max_length=255, null=True, blank=True)
+    addr = models.CharField(max_length=255, null=False, blank=False)
     additional_addr = models.CharField(max_length=50,
                                        null=False, blank=False)
+
+
+class Schedule(models.Model):
+    """Schedule for each organization work day."""
+    WEEK_DAY_CHOICES = (
+        (1, 'Domingo'),
+        (2, 'Segunda-feira'),
+        (3, 'Terça-feira'),
+        (4, 'Quarta-feira'),
+        (5, 'Quinta-feira'),
+        (6, 'Sexta-feira'),
+        (7, 'Sábado'),
+    )
+    time_from = models.TimeField(null=False, blank=False)
+    time_to = models.TimeField(null=False, blank=False)
+    week_day = models.IntegerField(null=False, choices=WEEK_DAY_CHOICES)
+    organization = models.ForeignKey(Organization,
+                                     on_delete=models.CASCADE,
+                                     related_name='schedules')
